ddb74f2ff1cc01c55ef1033f66b5ed940856935d
Eugenia Asikabeng <77032468+Eugenia-cloud@users.noreply.github.com>
2022-06-25 21:15:32 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,42 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_security_group" "ec2_public_security_group" {
+  name        = "EC2-Public-SG"
+  description = "internet reaching access for EC2 Instances"
+  vpc_id      = "vpc-060c8e0e0896c4544"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_instance" "jenkinsec2" {
+  instance_type   = var.instance_type
+  ami             = var.ami_id
+  key_name        = "tt"
+  security_groups = ["${aws_security_group.ec2_public_security_group.name}"]
+  user_data       = file("userdata.tpl")
+  tags = {
+    "Name" = "Auto-Jenkins",
+    LOB    = var.lob_dev
+  }
+}
+
