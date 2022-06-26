4972517601f5ecdce681d4d4a42529d97dd3722a
Eugenia Asikabeng <77032468+Eugenia-cloud@users.noreply.github.com>
2022-06-25 22:23:00 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_security_group" "ec2_public_security_group2" {
-  name        = "EC2-Public-SG"
+resource "aws_security_group" "jenkinsSG2" {
+  name        = "jenkinsSG2"
   description = "internet reaching access for EC2 Instances"
   vpc_id      = "vpc-060c8e0e0896c4544"
 
@@ -32,7 +32,7 @@ resource "aws_instance" "jenkins2" {
   instance_type   = var.instance_type
   ami             = var.ami_id
   key_name        = "tt"
-  security_groups = ["${aws_security_group.ec2_public_security_group2.name}"]
+  security_groups = ["${aws_security_group.jenkinsSG2.name}"]
   user_data       = file("userdata.tpl")
   tags = {
     "Name" = "Auto-Jenkins",
