6cb1332d431d210626a04a9b1e8400be48423814
Eugenia Asikabeng <77032468+Eugenia-cloud@users.noreply.github.com>
2022-06-25 21:16:17 -0400

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,4 @@
+variable "region" { default = "us-east-1" }
+variable "instance_type" { default = "t2.medium" }
+variable "ami_id" { default = "ami-052efd3df9dad4825" }
+variable "lob_dev" { default = "Jenkins-Dev" }
